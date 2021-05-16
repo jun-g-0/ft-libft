33c8d5c1b9cde5df9ad9f8c607c18348edcddd3f
jun-g-0 <junworks7@gmail.com>
2021-05-17 00:44:42 +0900

    feat: add ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,28 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/16 23:29:56 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/17 00:44:20 by jungao           ###   ########.fr       */
+/*   Created: 2021/05/17 00:33:24 by jungao            #+#    #+#             */
+/*   Updated: 2021/05/17 00:40:57 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*dst;
+	unsigned char	*dest_c;
+	unsigned char	*src_c;
 	int				i;
 
-	dst = (unsigned char *)s;
+	dest_c = (unsigned char *)dest;
+	src_c = (unsigned char *)src;
 	i = 0;
 	while (n > 0)
 	{
-		dst[i] = 0;
+		dest_c[i] = src_c[i];
 		i++;
 		n--;
 	}
+	return (dest);
 }
