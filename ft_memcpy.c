2001935ec3d6d8592a1b90ca7e01a420b11bb96a
jun-g-0 <junworks7@gmail.com>
2021-05-17 17:16:25 +0900

    fix: ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -21,12 +21,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	dest_c = (unsigned char *)dest;
 	src_c = (unsigned char *)src;
 	i = 0;
-	if (dest == NULL)
-	{
-		return (dest);
-	}
 	while (n > 0)
 	{
+		if (dest == NULL)
+		{
+			return (dest);
+		}
 		dest_c[i] = src_c[i];
 		i++;
 		n--;
