5f3aa5cc3710785d4fc0fd4d97e8ef1ebf93e477
jun-g-0 <junworks7@gmail.com>
2021-05-17 17:15:25 +0900

    fix: ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	dest_c = (unsigned char *)dest;
 	src_c = (unsigned char *)src;
 	i = 0;
-	if (dest == NULL || src == NULL)
+	if (dest == NULL)
 	{
 		return (dest);
 	}
