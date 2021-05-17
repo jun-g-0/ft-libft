df9be6a0b7271a70712d5e21fdc3a3a4c1c99597
jun-g-0 <junworks7@gmail.com>
2021-05-17 23:13:30 +0900

    fix: re-factor ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 22:55:31 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/17 23:09:48 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/17 23:13:15 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,22 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	int		i;
+	char	*save;
 
 	i = 0;
+	save = NULL;
 	while (s[i] != '\0')
 	{
+		if (s[i] == (char)c)
+		{
+			save = (char *)(s + i);
+		}
 		i++;
 	}
 	if ((char)c == '\0')
 	{
 		return ((char *)(s + i));
 	}
-	while (i != 0)
-	{
-		if (s[i] == (char)c)
-		{
-			return ((char *)(s + i));
-		}
-		i--;
-	}
-	return (NULL);
+	return (save);
 }
