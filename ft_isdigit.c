39f1699aad81e017aa0d8d46d9fec64a89d8bc3e
jun-g-0 <junworks7@gmail.com>
2021-05-18 17:33:51 +0900

    feat: WIP add ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 17:26:32 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/18 17:33:00 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/18 17:33:47 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,17 @@ int	ft_isdigit(int c)
 	}
 }
 
-// #include <ctype.h>
-// #include <stdio.h>
-// int main(int argc, char const *argv[])
-// {
-// 	char test1 = 'a';
-// 	char test2 = '5';
+#include <ctype.h>
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	char test1 = 'a';
+	char test2 = '5';
 
-// 	int result = isdigit(test1);
-// 	int result2 = isdigit('5');
+	int result = isdigit(test1);
+	int result2 = isdigit('5');
 
-// 	printf("%d, %d", result, result2);
-// 	return 0;
-// }
+	printf("%d, %d", result, result2);
+	return 0;
+}
 
