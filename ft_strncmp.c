d8e01531664d6747badc6ee27a4a5a6260fb6b96
jun-g-0 <junworks7@gmail.com>
2021-05-19 01:04:44 +0900

    save tests

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:37:54 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 00:56:46 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 01:04:39 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,31 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return ((unsigned char)0);
 	}
 	i = 0;
-	while (n - i > 0 && s1[i] != '\0' && s2[i] != '\0')
+	while (n - i > 0 && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 	}
-	if (s1[i] != '\0' || s2[i] != '\0')
-	{
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	}
-	else
+	if (n - i == 0)
 	{
 		return ((unsigned char)0);
 	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-
 
 #include <string.h>
 #include <stdio.h>
 
 int main()
 {
-	char str1[5] = "abc5";
-	char str2[5] = "abc8";
+	char str1[5] = "ab";
+	char str2[5] = "abc";
 	char str3[5] = "";
 
-	printf("or: %d\n", strncmp(str1, str3, 0));
-	printf("ft: %d\n", ft_strncmp(str1, str3, 0));
-	printf("or: %d\n", strncmp(str1, str2, 3));
-	printf("ft: %d\n", ft_strncmp(str1, str2, 3));
-	printf("or: %d\n", strncmp(str1, str2, 4));
-	printf("ft: %d\n", ft_strncmp(str1, str2, 4));
-	printf("or: %d\n", strncmp(str1, str3, 4));
-	printf("ft: %d\n", ft_strncmp(str1, str3, 4));
+	for (size_t i = 0; i < 5; i++)
+	{
+		printf("or: %d\n", strncmp(str1, str2, i));
+		printf("ft: %d\n", ft_strncmp(str1, str2, i));
+		printf("or: %d\n", strncmp(str1, str3, i));
+		printf("ft: %d\n", ft_strncmp(str1, str3, i));
+	}
 }
