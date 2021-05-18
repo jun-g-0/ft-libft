65dbff3292e2e8fda2f0cdd70e624a4573211860
jun-g-0 <junworks7@gmail.com>
2021-05-19 00:48:12 +0900

    fix?

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:37:54 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 00:45:33 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 00:48:05 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,26 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		}
 		i++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	if (n == 0)
+	{
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return ((unsigned char)0);
+}
+
+
+#include <string.h>
+#include <stdio.h>
+int main()
+{
+	char str1[5] = "abc5";
+	char str2[5] = "abc8";
+	char str3[5] = "";
+
+	printf("or: %d\n", strncmp(str1, str2, 3));
+	printf("ft: %d\n", ft_strncmp(str1, str2, 3));
+	printf("or: %d\n", strncmp(str1, str2, 4));
+	printf("ft: %d\n", ft_strncmp(str1, str2, 4));
+	printf("or: %d\n", strncmp(str1, str3, 4));
+	printf("ft: %d\n", ft_strncmp(str1, str3, 4));
 }
