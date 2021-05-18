90c2f764ecc3d1d5e9694002ddb6be401694c534
jun-g-0 <junworks7@gmail.com>
2021-05-19 00:37:25 +0900

    fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:37:54 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 00:31:54 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 00:37:22 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	if (n == 0)
 	{
-		return (0);
+		return ((unsigned char)0);
 	}
 	i = 0;
 	while (n - i > 0 && s1[i] != '\0' && s2[i] != '\0')
@@ -29,5 +29,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		}
 		i++;
 	}
-	return (0);
+	return ((unsigned char)0);
 }
