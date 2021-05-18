7792fb416884419c1392efdb7f33786433e8eab7
jun-g-0 <junworks7@gmail.com>
2021-05-18 16:36:56 +0900

    feat: add ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,13 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 23:15:18 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/18 02:24:47 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/18 16:34:40 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
+static size_t	my_strlen(const char *s);
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -26,26 +27,27 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (len - i > 0 && haystack[i] != '\0')
 	{
 		j = 0;
-		while (len - (i + j) > 0 && haystack[i + j] == needle[j])
+		while (len - (i + j) > 0 && haystack[(i + j)] == needle[j])
 		{
-			if (j == (int)ft_strlen(needle))
+			j++;
+			if (j == (int)my_strlen(needle))
 			{
 				return ((char *)(haystack + i));
 			}
-			j++;
 		}
 		i++;
 	}
 	return (NULL);
 }
 
-// int main()
-// {
-// 	// ft_strnstr
-// 	printf("\n---ft_strnstr\n");
-// 	char str19[7] = "string";
-// 	char str20[3] = "tr";
+static size_t	my_strlen(const char *s)
+{
+	size_t	n;
 
-// 	char *result6 = ft_strnstr(str19, str20, (size_t)7);
-// 	printf("%s\n", result6);
-// }
+	n = 0;
+	while (s[n])
+	{
+		n++;
+	}
+	return (n);
+}
