207dd79eda3d5c15e5b1fe5b95158a77c1fe426a
jun-g-0 <junworks7@gmail.com>
2021-05-18 19:50:50 +0900

    fix: fix update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,22 +6,96 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:58:38 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/18 17:26:04 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/18 19:34:32 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
+int			my_isdigit(int c);
+int			my_isspace(char c);
+long int	overflow(char c, long int *result, int sign);
 
 int	ft_atoi(const char *nptr)
 {
-	return (atoi(nptr));
+	int			sign;
+	long int	result;
+
+	sign = 1;
+	result = 0;
+	while (my_isspace(*nptr))
+		nptr++;
+	if (*nptr == '-')
+		sign = -1;
+	if (*nptr == '-' || *nptr == '+')
+		nptr++;
+	while (my_isdigit(*nptr))
+	{
+		if (overflow(*nptr, &result, sign))
+		{
+			return ((int)overflow(*nptr, &result, sign));
+		}
+		nptr++;
+	}
+	return ((int)(result * sign));
+}
+
+int	my_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+int	my_isspace(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\v')
+	{
+		return (1);
+	}
+	else if (c == '\f' || c == '\r' || c == ' ')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+long int	overflow(char c, long int *result, int sign)
+{
+	long int	ov_div;
+	long int	ov_mod;
+	
+	if (sign > 0)
+	{
+		ov_div = LONG_MAX / 10;
+		ov_mod = LONG_MAX % 10;
+		if (*result > ov_div || (*result == ov_div && (int)c > ov_mod))
+			return (LONG_MAX);
+	}
+	else
+	{
+		ov_div = ((LONG_MIN / 10) * sign);
+		ov_mod = ((LONG_MIN % 10) * sign);
+		if (*result > ov_div || (*result == ov_div && (int)c > ov_mod))
+			return (LONG_MIN);
+	}
+	*result = *result * 10 + ((long int)c * sign);
+	return (0);
 }
 
 int	main()
 {
-	char *str = "   + 10.";
+	char *str = "   +10.";
 	int result = ft_atoi(str);
 	printf("%d\n", result);
 }
--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 17:26:32 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/18 17:33:47 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/18 17:34:50 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,3 @@ int	ft_isdigit(int c)
 		return (0);
 	}
 }
-
-#include <ctype.h>
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-	char test1 = 'a';
-	char test2 = '5';
-
-	int result = isdigit(test1);
-	int result2 = isdigit('5');
-
-	printf("%d, %d", result, result2);
-	return 0;
-}
-
