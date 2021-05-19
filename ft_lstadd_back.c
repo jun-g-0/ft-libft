6e9bea30f4f87d21f2cc4961d31a1d5966a76880
Jun Gao <junworks7@gmail.com>
2021-05-19 22:05:48 +0900

    fix: ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 21:50:55 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 22:04:48 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 22:05:31 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*tmp;
 
-	if (lst != NULL)
+	if (lst[0] != NULL)
 	{
-		if (lst[0] != NULL)
-		{
-			tmp = ft_lstlast(lst[0]);
-			tmp->next = new;
-		}
-		else
-		{
-			lst[0] = new;
-		}
+		tmp = ft_lstlast(lst[0]);
+		tmp->next = new;
+	}
+	else
+	{
+		lst[0] = new;
 	}
 }
