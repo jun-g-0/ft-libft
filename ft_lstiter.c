081595c269ed47c56ad42a0a99a6c5b9ee3274c8
Jun Gao <junworks7@gmail.com>
2021-05-19 22:31:19 +0900

    fix: ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 22:27:34 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 22:30:17 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 22:31:01 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	while (lst != NULL)
+	while (lst->next != NULL)
 	{
 		f(lst->content);
 		lst = lst->next;
