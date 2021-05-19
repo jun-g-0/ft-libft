41fd292e0915257b248adcafc58fa2172e9e14da
jun-g-0 <junworks7@gmail.com>
2021-05-19 14:38:52 +0900

    fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 12:18:32 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 14:37:42 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 14:38:47 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 static int		my_include(char c, char const *set);
 static size_t	my_strlen(const char *s);
 
+#include <stdio.h>
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*new_str;
@@ -22,13 +23,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		i;
 
 	start = 0;
+	printf("line 26\n");
 	while (s1[start] != '\0' && my_include(s1[start], set))
 		start++;
+	printf("line 29\n");
 	end = my_strlen(s1);
 	while (end > 0 && my_include(s1[(end - 1)], set))
 		end--;
+	printf("line 33\n");
 	if (!(new_str = malloc(sizeof(char) * (end - start + 1))))
 		return (NULL);
+	printf("line 36\n");
 	i = 0;
 	while (i + start < end)
 	{
