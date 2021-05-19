2bdb008073c2b2ce7ff0896e49ef1de1ae33690d
jun-g-0 <junworks7@gmail.com>
2021-05-19 14:24:13 +0900

    fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,47 +6,78 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 12:18:32 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 14:11:32 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 14:24:08 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+static int		my_include(char c, char const *set);
+static size_t	my_strlen(const char *s);
 
-static int
-	ft_char_in_set(char c, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
+	char	*new_str;
+	int		start;
+	int		end;
+	int		i;
+
+	start = 0;
+	while (s1[start] != '\0' && my_include(s1[start], set))
+		start++;
+	end = my_strlen(s1);
+	while (end > 0 && my_include(s1[end - 1], set))
+		end--;
+	if (!(new_str = malloc(sizeof(char) * (end - start + 1))))
+		return (NULL);
+	i = 0;
+	while (i + start < end)
+	{
+		new_str[i] = s1[i + start];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+static int	my_include(char c, char const *set)
+{
+	int	i;
 
 	i = 0;
-	while (set[i])
+	while (set[i] != '\0')
 	{
 		if (set[i] == c)
+		{
 			return (1);
+		}
 		i++;
 	}
 	return (0);
 }
 
-char
-	*ft_strtrim(char const *s1, char const *set)
+static size_t	my_strlen(const char *s)
 {
-	char	*str;
-	size_t	i;
-	size_t	start;
-	size_t	end;
+	size_t	n;
 
-	start = 0;
-	while (s1[start] && ft_char_in_set(s1[start], set))
-		start++;
-	end = ft_strlen(s1);
-	while (end > start && ft_char_in_set(s1[end - 1], set))
-		end--;
-	str = (char*)malloc(sizeof(*s1) * (end - start + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (start < end)
-		str[i++] = s1[start++];
-	str[i] = 0;
-	return (str);
+	n = 0;
+	while (s[n])
+	{
+		n++;
+	}
+	return (n);
 }
+
+// #include <stdio.h>
+
+// int main(int argc, char const *argv[])
+// {
+// 	printf("%d, %s\n", argc, argv[0]);
+// 	char *str1 = "";
+// 	char *str2 = " ";
+	
+// 	char *result = ft_strtrim(str1, str2);
+// 	printf("%s\n", result);
+// 	printf("%p\n", result);
+// 	printf("%d%d\n", (int)result[0], (int)result[1]);
+// 	return 0;
+// }
