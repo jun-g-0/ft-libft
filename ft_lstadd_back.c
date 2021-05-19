717cfa57599bcf3c9230c9cdedc012a280f70f4e
Jun Gao <junworks7@gmail.com>
2021-05-19 22:00:45 +0900

    wip

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 21:50:55 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 21:54:29 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 22:00:39 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 
 	if (lst == NULL)
 	{
-		return (NULL);
-	}
-	if (lst[0] == NULL)
-	{
-		return (NULL);
+		lst[0] = new;
 	}
 	tmp = lst[0];
 	if (tmp->next != NULL)
