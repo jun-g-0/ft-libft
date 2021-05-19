f73efd83b9eb2c2b65d0a32279265ce3f24d72c0
Jun Gao <junworks7@gmail.com>
2021-05-19 21:37:55 +0900

    wip

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 14:48:06 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 21:30:42 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 21:33:13 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
-// void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 // int		ft_lstsize(t_list *lst);
 // t_list	*ft_lstlast(t_list *lst);
 // void	ft_lstadd_back(t_list **lst, t_list *new);
