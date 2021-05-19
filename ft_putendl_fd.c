5f7f36870300625dd5734e57a3375c1806c0a5a0
Jun Gao <junworks7@gmail.com>
2021-05-19 19:43:59 +0900

    wip

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,25 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/19 19:24:28 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 19:43:55 by jungao           ###   ########.fr       */
+/*   Created: 2021/05/19 19:40:17 by jungao            #+#    #+#             */
+/*   Updated: 2021/05/19 19:43:30 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
