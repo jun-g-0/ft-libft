f7ca4f9e26c1e8e33d4054ec46be9e5efad29d70
jun-g-0 <junworks7@gmail.com>
2021-05-19 18:44:09 +0900

    wip

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 17:39:54 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 18:32:37 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 18:43:59 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ char	*ft_itoa(int n)
 	if (result == NULL)
 		return (NULL);
 	result[count--] = '\0';
-	while (count > 0)
+	if (n < 0)
+		result[0] = '-';
+	while (n != 0)
 	{
-		result[count--] = n % 10;
+		result[count--] = (char)((n % 10) - '0');
 		n = n / 10;
 	}
 	return (result);
@@ -43,4 +45,22 @@ int		my_get_count(int n)
 		n = n / 10;
 	}
 	return num;
+}
+
+#include <stdio.h>
+#include <limits.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("%d, %s\n", argc, argv[0]);
+	int ints[5] = {-623, 156, -0, INT_MAX, INT_MIN};
+	
+	for (size_t i = 0; i < 5; i++)
+	{
+		printf("%d: %s\n", ints[i], ft_itoa(ints[i]));
+	}
+	
+	// char *result = ft_substr(str1, 5, 20);
+	// printf("%s\n", result);
+	return 0;
 }
