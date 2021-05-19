a6663be7b90bdc012c06a7610862c3c2cde6a89e
jun-g-0 <junworks7@gmail.com>
2021-05-19 10:36:53 +0900

    feat: rm tests

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 10:08:41 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 10:36:21 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 10:36:46 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,23 +43,5 @@ void	my_bzero(void *s, size_t n)
 		dst[i] = 0;
 		i++;
 		n--;
-	}
-}
-
-#include <stdlib.h>
-#include <stdio.h>
-int main()
-{
-	for (size_t i = 0; i < 3; i++)
-	{
-		for (size_t j = 0; j < 3; j++)
-		{
-			printf("i = %zu, j = %zu\n", i, j);
-			char *result1 = calloc(i, j);
-			char *result2 = ft_calloc(i, j);
-			printf("o = %p, f = %p\n", result1, result2);
-			printf("o = %d%d%d%d%d\n", result1[0], result1[1], result1[2], result1[3], result1[4]);
-			printf("f = %d%d%d%d%d\n", result2[0], result2[1], result2[2], result2[3], result2[4]);
-		}
 	}
 }
