1c99bc62a2d02a34b514fce556600e9d9b222609
Jun Gao <junworks7@gmail.com>
2021-05-19 21:50:32 +0900

    feat: add ft_lstlast.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/14 14:48:06 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 21:42:29 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 21:49:06 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ typedef struct s_list
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
-// t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 // void	ft_lstadd_back(t_list **lst, t_list *new);
 // void	ft_lstdelone(t_list *lst, void (*del)(void*));
 // void	ft_lstclear(t_list **lst, void (*del)(void*));
