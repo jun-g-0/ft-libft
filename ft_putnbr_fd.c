e840de59a83125dc7db10741bdf761c2a32f0565
Jun Gao <junworks7@gmail.com>
2021-05-19 20:04:19 +0900

    wip

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,49 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 19:44:49 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 19:45:42 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 20:04:15 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+static void	my_rec_ptnbr(int n, int fd);
+static int	my_abs(int n);
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n < 0)
+		ft_putchar_fd('-', fd);
+	if (n == 0)
+	{
+		ft_putchar_fd('0', fd);
+	}
+	else
+	{
+		my_rec_ptnbr(n, fd);
+	}
+}
+
+static void	my_rec_ptnbr(int n, int fd)
+{
+	char	mod;
+
+	if (n != 0)
+	{
+		mod = (char)(my_abs(n % 10) + '0');
+		n = n / 10;
+		my_rec_ptnbr(n, fd);
+		ft_putchar_fd(mod, fd);
+	}
+}
+
+static int	my_abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
 }
