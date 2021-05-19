3b651888d6f052c9919501b7d31ef2b6ca40d83a
Jun Gao <junworks7@gmail.com>
2021-05-19 19:39:40 +0900

    fix: ft_putstr_fd to un protected

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 19:24:28 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 19:38:33 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/19 19:39:10 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ void	ft_putstr_fd(char *s, int fd)
 {
 	int	i;
 
-	if (s != NULL)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		i = 0;
-		while (s[i] != '\0')
-		{
-			write(fd, &s[i], 1);
-			i++;
-		}
+		write(fd, &s[i], 1);
+		i++;
 	}
 }
