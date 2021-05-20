c17a894fdffbdeed6a1b879581e917b8ea9126de
Jun Gao <junworks7@gmail.com>
2021-05-20 10:20:40 +0900

    fix: ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jungao <jungao@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 10:55:39 by jungao            #+#    #+#             */
-/*   Updated: 2021/05/19 14:54:50 by jungao           ###   ########.fr       */
+/*   Updated: 2021/05/20 10:20:10 by jungao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*new_str;
 	unsigned int	i;
 	unsigned int	j;
+	size_t			s_len;
 
 	i = 0;
 	j = 0;
-	new_str = malloc(sizeof(char) * len + 1);
-	if (!new_str)
+	s_len = ft_strlen(s);
+	if (len > s_len - start)
 	{
-		return (NULL);
+		len = s_len - start;
 	}
+	new_str = malloc(sizeof(char) * len + 1);
+	if (new_str == NULL)
+		return (NULL);
 	while (s[i] != '\0')
 	{
 		if (i >= start && len > j)
